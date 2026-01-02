deaf51f67513a39816ccafb3c11c3de82d8c2258
luniksss <nikolaeval316@gmail.com>
2026-01-02 20:03:27 +0300

    удалила ненужные файлы

--- pkg/data/mysql/migrations/20251022220910_create_outlet_table_down.sql
+++
@@ -1,5 +0,0 @@
-DROP INDEX IF EXISTS idx_product_stock_outlet ON product_stock;
-DROP INDEX IF EXISTS idx_product_stock_outlet ON product_stock;
-
-DROP TABLE IF EXISTS product_stock;
-DROP TABLE IF EXISTS sales_outlet;
