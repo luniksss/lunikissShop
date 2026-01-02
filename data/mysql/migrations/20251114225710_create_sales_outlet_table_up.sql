7824647ab23117f01a296144e2d49873b7822b6a
luniksss <nikolaeval316@gmail.com>
2026-01-02 19:59:27 +0300

    добавила миграции для бд

---
+++ data/mysql/migrations/20251114225710_create_sales_outlet_table_up.sql
@@ -0,0 +1,10 @@
+CREATE TABLE `sales_outlet`
+(
+    `id`         INT            NOT NULL AUTO_INCREMENT,
+    `address`    VARCHAR(255)   NOT NULL,
+    PRIMARY KEY (`id`)
+)
+    ENGINE = InnoDB
+    CHARACTER SET = utf8mb4
+    COLLATE utf8mb4_unicode_ci
+;
